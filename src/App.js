cece8ad6e26e897e323b32b8e047544c19a26217
xlogz <loganxlow@gmail.com>
2021-04-14 17:09:48 -0700

    Added board creator w/ random tiles set to on and off

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import logo from './logo.svg';
 import Board from './components/Board'
 import './App.css';
 
+const BOARD_WIDTH=5;
+const BOARD_HEIGHT=5;
+
 function App() {
+
+  function createBoard(w,h){
+    const board = [];
+    let newRow = [];
+
+    for(var i = 0; i < h; i++){
+      for(var b = 0; b < w; b++){
+        newRow.push(Math.round(Math.random()) ? true : false);
+      }
+      board.push(newRow);
+      newRow = [];
+    }
+    return board;
+  }
+
   return (
     <Board
-      board={[
-        [true,false,true,false,true],
-        [false,false,true,false,true],
-        [true,false,true,false,true],
-        [true,true,true,false,true],
-        [true,false,true,false,true],
-      ]}
+      board={createBoard(BOARD_WIDTH,BOARD_HEIGHT)}
     />
   );
 }
