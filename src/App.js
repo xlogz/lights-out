3eb0f83390fac4a9715a0e89088341ebe45347a1
xlogz <loganxlow@gmail.com>
2021-04-14 17:02:30 -0700

    Added board to main app screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
+import Board from './components/Board'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Board
+      board={[
+        [true,false,true,false,true],
+        [false,false,true,false,true],
+        [true,false,true,false,true],
+        [true,true,true,false,true],
+        [true,false,true,false,true],
+      ]}
+    />
   );
 }
 
