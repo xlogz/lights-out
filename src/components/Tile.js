b30277b858bc124f369efb5e36a95265a6130bd3
xlogz <loganxlow@gmail.com>
2021-04-14 17:20:04 -0700

    Adjusted conditional styling

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,23 +1,22 @@
 const Tile = (props) => {
     return (
-        <div style={ props.on ? styles.on : styles.off }></div>
+        <div style={ props.on ? {...styles.on, ...styles.tile} : {...styles.off, ...styles.tile} }></div>
     )
 }
 
 const styles = {
-    on: {
+    tile: {
+        border: "5px solid black",
+        display: "inline-block",
+        margin: "3px",
         width: 100,
         height: 100,
+    },
+    on: {
         backgroundColor: 'white',
-        border: "1px solid black",
-        display: "inline-block"
     },
     off:{
-        width: 100,
-        height: 100,
         backgroundColor: 'black',
-        border: "1px solid black",
-        display: "inline-block"
     }
 }
 
