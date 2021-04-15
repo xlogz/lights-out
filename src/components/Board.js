9dd63e645bea3a061c7aba7174c088f2dab80453
xlogz <loganxlow@gmail.com>
2021-04-14 18:01:20 -0700

    added destructuring to props object

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,16 +1,16 @@
 import Tile from './Tile';
 
-const Board = (props) =>{
+const Board = ({board, toggleTiles}) =>{
     return (
-        props.board.map((row,y)=>{
+        board.map((row,y)=>{
             const newRow = row.map((isOn,x)=>{
                 return (<Tile 
                     on={isOn}
                     x={x}
                     y={y}
                     key={`${x} ${y}`}
-                    toggleTiles={props.toggleTiles}
-                    board={props.board}
+                    toggleTiles={toggleTiles}
+                    board={board}
                     />)
             });
             return (<div key={`${y}`} style={styles.row}>{newRow}</div>)
