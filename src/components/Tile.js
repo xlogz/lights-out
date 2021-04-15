03352a0ee712f1a798986b5df3188374cf3e40da
xlogz <loganxlow@gmail.com>
2021-04-14 17:03:03 -0700

    Added inline-block settings to Tile

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,6 +1,6 @@
 const Tile = (props) => {
     return (
-        <div style={ props.on ? on : off }></div>
+        <div style={ props.on ? styles.on : styles.off }></div>
     )
 }
 
@@ -9,13 +9,15 @@ const styles = {
         width: 100,
         height: 100,
         backgroundColor: 'white',
-        border: "1px solid black"
+        border: "1px solid black",
+        display: "inline-block"
     },
     off:{
         width: 100,
         height: 100,
         backgroundColor: 'black',
-        border: "1px solid black"
+        border: "1px solid black",
+        display: "inline-block"
     }
 }
 
